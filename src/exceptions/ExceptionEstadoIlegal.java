b40c0bdc8ca2c8beab0d37382396165544e84e62
Nickydos123 <mcopticigna389@gmail.com>
2025-10-30 16:21:56 -0300

    creacion de exceptions personalizadas

---
+++ src/exceptions/ExceptionEstadoIlegal.java
@@ -0,0 +1,8 @@
+package exceptions;
+
+public class ExceptionEstadoIlegal extends RuntimeException {
+    public ExceptionEstadoIlegal(String message) {
+        super(message);
+    }
+
+}
---
+++ src/exceptions/ExceptionHabitacionNoDisponible.java
@@ -0,0 +1,8 @@
+package exceptions;
+
+public class ExceptionHabitacionNoDisponible extends RuntimeException {
+    public ExceptionHabitacionNoDisponible(String message) {
+        super(message);
+    }
+
+}
