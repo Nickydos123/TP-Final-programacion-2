0042e299405f75eb0295058613d4fd872d69aafd
Nickydos123 <mcopticigna389@gmail.com>
2025-10-30 17:37:46 -0300

    implementacion removeUsuario en administrador y en IManejaUsuarios

--- src/interfaces/IManejaUsuarios.java
+++ src/interfaces/IManejaUsuarios.java
@@ -2,4 +2,5 @@ package interfaces;
 
 public interface IManejaUsuarios {
     void addUsuario(Usuario usuario, Hotel hotel);
+    void removeUsuario(Usuario usuario, Hotel hotel);
 }
--- src/modelo/Administrador.java
+++ src/modelo/Administrador.java
@@ -1,12 +1,23 @@
 package modelo;
 
+import interfaces.IManejaUsuarios;
+
 public class Administrador extends Usuario implements IManejaUsuarios, IHaceBackups{
+    public Administrador(String nombre, String apellido, String dni, String domicilio, String userName, String password) {
+        super(nombre, apellido, dni, domicilio, userName, password);
+    }
+
     @Override
     public void addUsuario(Usuario usuario, Hotel hotel) {
         hotel.getUsuarios().add(usuario);
     }
     @Override
+    public void removeUsuario(Usuario usuario, Hotel hotel) {
+        hotel.getUsuarios().remove(usuario);
+    }
+    @Override
     public void realizarBackup(Backupper backupper){
         backupper.realizarBackup();
     }
+
 }
