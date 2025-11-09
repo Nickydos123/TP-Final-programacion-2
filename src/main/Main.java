4f3647591ad475b737f79998bc0876be2c40179a
Marc0sH <marcoshoyuelos@gmail.com>
2025-11-09 12:37:05 -0300

    Este commit agrega funcionalida to/fromJson a las 4 clases que lo van a necesitar. Ademas de agregar alguna facultades al administradaor y recepcionista.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,30 +1,55 @@
 package main;
 
-import modelo.Administrador;
-import modelo.Hotel;
-import modelo.Sistema;
-import modelo.Usuario;
+import enums.EestadoHabitacion;
+import enums.EtipoHabitacion;
+import modelo.*;
+
+import java.time.LocalDate;
 
 public class Main {
     static void main() {
         //Esto son solo pruebas luego aca deberia ir una clase menu
         Hotel hotel = new Hotel();
-        Administrador administrador = new Administrador("Juan","Encargadovich","112435345","Calle 123","admin","admin123");
-        Sistema sistema = new Sistema(hotel,administrador);
+        Sistema sistema = new Sistema(hotel);
+        Administrador startingAdmin = new Administrador("Juan","Adminovich","3344556","Calle 123","admin","admin123",sistema);
+        startingAdmin.addUsuario(startingAdmin);
 
         sistema.login("admin","admin123");
 
         if(sistema.isAdminLoggedIn()){
-            System.out.println("Bienvenido Administrador");
+            System.out.println("Bienvenido Administrador " + sistema.getCurrentUser().getNombre());
             Administrador currentAdmin = (Administrador) sistema.getCurrentUser();
 
-            currentAdmin.addUsuario(new Usuario("Paco","Perez","353453456","Calle456","xXPacoXx","paco123"),sistema);
+            currentAdmin.addUsuario(new Usuario("Paco","Perez","456456456","Calle345","xXpacoXx","paco123",currentAdmin.getSistema()));
+            currentAdmin.asignarTipo("xXpacoXx","recepcionista");
 
-            //Hacen falta mas metodos en recepcionista y admin para manejar el tema de la muestra de datos(Esto de abajo es un prueba)
-            System.out.println(sistema.getUsuarios().get("xXPacoXx").toString());
+            currentAdmin.addHabitacion(new Habitacion(EtipoHabitacion.SENCILLA,3000, EestadoHabitacion.DISPONIBLE));
+            currentAdmin.addHabitacion(new Habitacion(EtipoHabitacion.SENCILLA,3000, EestadoHabitacion.DISPONIBLE));
+            currentAdmin.addHabitacion(new Habitacion(EtipoHabitacion.SUITE,3000, EestadoHabitacion.DISPONIBLE));
         }
 
+        sistema.logout();
+        sistema.login("xXpacoXx","paco123");
 
+        if(sistema.isRecepcionistaLoggedIn()){
+            System.out.println("Bienvenido Recepcionista " + sistema.getCurrentUser().getNombre());
+            Recepcionista currentRecepcionista = (Recepcionista) sistema.getCurrentUser();
+
+            System.out.println(currentRecepcionista.mostrarHabitaciones(sistema.getHotel()));
+
+            currentRecepcionista.registraReserva(sistema.getHotel(),1,"44635116", LocalDate.of(2002,12,16),LocalDate.of(2002,12,20));
+            currentRecepcionista.registraReserva(sistema.getHotel(),1,"254234534",LocalDate.of(2002,12,20), LocalDate.of(2002,12,25));
+            //currentRecepcionista.registraReserva(sistema.getHotel(),1,"456745646",LocalDate.of(2002,12,17), LocalDate.of(2002,12,25)); //Funciona el solapamiento de fechas
+
+            currentRecepcionista.hacerCheckIn(sistema.getHotel(),1);
+            currentRecepcionista.registraReserva(sistema.getHotel(),2,"456745646",LocalDate.of(2002,12,17), LocalDate.of(2002,12,25)); //Funciona el solapamiento de fechas
+
+
+            System.out.println(currentRecepcionista.mostrarReservas(sistema.getHotel()));
+            System.out.println(currentRecepcionista.mostrarEstadias(sistema.getHotel()));
+
+            System.out.println(currentRecepcionista.hacerCheckOut(sistema.getHotel(),1).toString());//Cuando hago el checkout retorno la reserva terminada
+        }
     }
 
 }
--- src/modelo/Usuario.java
+++ src/modelo/Usuario.java
@@ -1,12 +1,57 @@
 package modelo;
 
+import org.json.JSONObject;
+
 public class Usuario extends Persona{
     protected String userName;//Identificador para los usuarios
     protected String password;
-    public Usuario(String nombre, String apellido, String dni, String domicilio, String userName, String password) {
+    protected Sistema sistema; // Sistema al que el usuario pertenece. Quiero un sistema por usuario
+
+    public Usuario(String nombre, String apellido, String dni, String domicilio, String userName, String password, Sistema sistema) {
         super(nombre, apellido, dni, domicilio);
         this.userName = userName;
         this.password = password;
+        this.sistema = sistema;
+    }
+
+    public static JSONObject toJson(Usuario u) {
+        JSONObject obj = new JSONObject();
+        obj.put("nombre", u.getNombre());
+        obj.put("apellido", u.getApellido());
+        obj.put("dni", u.getDni());
+        obj.put("domicilio", u.getDomicilio());
+        obj.put("userName", u.getUserName());
+        obj.put("password", u.getPassword());
+
+        //Agrego un campo extra al Json de Usuarios para no olvidarme de que tipo especifico eran
+        if (u instanceof Recepcionista){
+            obj.put("tipo", "Recepcionista");
+        }else if (u instanceof Administrador) {
+            obj.put("tipo", "Administrador");
+        } else {
+            obj.put("tipo", "Usuario");
+        }
+        return obj;
+    }
+
+    public static Usuario fromJson(JSONObject obj, Sistema sistema) {
+        String tipo = obj.getString("tipo");
+        String nombre = obj.getString("nombre");
+        String apellido = obj.getString("apellido");
+        String dni = obj.getString("dni");
+        String domicilio = obj.getString("domicilio");
+        String userName = obj.getString("userName");
+        String password = obj.getString("password");
+
+        //Me fijo en el parametro tipo para poder instanciar al Usuario como lo que realmente era
+        switch (tipo) {
+            case "Recepcionista":
+                return new Recepcionista(nombre, apellido, dni, domicilio, userName, password, sistema);
+            case "Administrador":
+                return new Administrador(nombre, apellido, dni, domicilio, userName, password, sistema);
+            default:
+                return new Usuario(nombre, apellido, dni, domicilio, userName, password, sistema);
+        }
     }
 
     protected String getUserName() {
@@ -23,6 +68,14 @@ public class Usuario extends Persona{
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public Sistema getSistema() {
+        return sistema;
+    }
+
+    public void setSistema(Sistema sistema) {
+        this.sistema = sistema;
     }
 
     @Override
