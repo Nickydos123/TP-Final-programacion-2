9785f9655ea9182f942cc4c6182915ea2467f371
Marc0sH <marcoshoyuelos@gmail.com>
2025-11-10 15:18:20 -0300

    Logica en recepcionista para poder poner habitaciones en mantenimiento.

--- src/interfaces/IHacerCheckInyCheckOut.java
+++ src/interfaces/IHacerCheckInyCheckOut.java
@@ -4,6 +4,6 @@ import modelo.Estadia;
 import modelo.Hotel;
 
 public interface IHacerCheckInyCheckOut {
-   Estadia hacerCheckIn(Hotel hotel, int reservaId);
-    Estadia hacerCheckOut(Hotel hotel, int estadiaId);
+   Estadia hacerCheckIn(int reservaId);
+    Estadia hacerCheckOut(int estadiaId);
 }
